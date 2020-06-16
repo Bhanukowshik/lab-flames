e50258cd234d813741543f00c8122e1a1f196ea3
P.Bhanu <kowshikbhanu1000@gmail.com>
2020-06-16 22:50:56 +0530

    Flames

--- src/controller/FlamesCheck.java
+++ src/controller/FlamesCheck.java
@@ -1,9 +1,6 @@
 package controller;
 
-/*
-1.Do not modify the existing code at any point or you will not get output.
-2. Uncomment the code inside the doPost method given in this class to run and see the output
-*/
+
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
@@ -13,55 +10,68 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import service.FlamesCheckService;
 
-
+/**
+ * Servlet implementation class FlamesCheck
+ */
 @WebServlet("/flames")
 public class FlamesCheck extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FlamesCheck() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
-	public FlamesCheck() {
-		super();
-
-	}
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		RequestDispatcher rd = this.getServletContext().getRequestDispatcher("/WEB-INF/views/check.jsp");
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/check.jsp");
 		rd.forward(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-
-		/*
-		 * Uncomment the below code to test your output 
-		 * String name1 = request.getParameter("your"); 
-		 * String name2 = request.getParameter("crush");
-		 * 
-		 * FlamesCheckService fcs = new FlamesCheckService();
-		 * 
-		 * char k = fcs.findFlames(name1,name2);
-		 * 
-		 * if (k == 'f') { RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/friends.jsp"
-		 * ); rd.forward(request, response);
-		 * 
-		 * } else if (k == 'l') { RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/lovers.jsp")
-		 * ; rd.forward(request, response);
-		 * 
-		 * } else if (k == 'a') { RequestDispatcher rd=this.getServletContext().getRequestDispatcher(
-		 * "/WEB-INF/views/affection.jsp"); rd.forward(request, response);
-		 * 
-		 * } else if (k == 'm') { RequestDispatcher rd=this.getServletContext().getRequestDispatcher(
-		 * "/WEB-INF/views/marriage.jsp"); rd.forward(request, response);
-		 * 
-		 * } else if (k == 'e') { RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/enemies.jsp"
-		 * ); rd.forward(request, response);
-		 * 
-		 * } else if (k == 's') { RequestDispatcher rd=this.getServletContext().getRequestDispatcher(
-		 * "/WEB-INF/views/siblings.jsp"); rd.forward(request, response);
-		 * 
-		 * }
-		 */
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String name1 = request.getParameter("your");
+		String name2 = request.getParameter("crush");
+		
+		FlamesCheckService fcs = new FlamesCheckService();
+					
+		char k = fcs.findFlames(name1,name2);
+		
+				if (k == 'f') {
+					RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/friends.jsp");
+					rd.forward(request, response);
+				//	response.sendRedirect("/WEB-INF/views/friends.jsp");
+				} else if (k == 'l') {
+					RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/lovers.jsp");
+					rd.forward(request, response);
+				//	response.sendRedirect("/WEB-INF/views/lovers.jsp");
+				} else if (k == 'a') {
+					RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/affection.jsp");
+					rd.forward(request, response);
+				//	response.sendRedirect("/WEB-INF/views/affection.jsp");
+				} else if (k == 'm') {
+					RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/marriage.jsp");
+					rd.forward(request, response);
+				//	response.sendRedirect("/WEB-INF/views/marriage.jsp");
+				} else if (k == 'e') {
+					RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/enemies.jsp");
+					rd.forward(request, response);
+				//	response.sendRedirect("/WEB-INF/views/enemies.jsp");
+				} else if (k == 's') {
+					RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/siblings.jsp");
+					rd.forward(request, response);
+				//	response.sendRedirect("/WEB-INF/views/siblings.jsp");
+				}
 	}
 
 }
--- src/service/FlamesCheckService.java
+++ src/service/FlamesCheckService.java
@@ -1,9 +1,120 @@
 package service;
 
-// Create a class called FlamesCheckService
-// FlamesCheckService has a method findFlames to find the flames between two names
-// char findFlames(String name1, String name2) takes two strings as arguments
-// Your task is to calculate the flames value and return the corresponding character as output 
-// You must return only the following values ['f','l','a','m','e','s']
-// change the return value at the end of the method corresponding to your return value
+public class FlamesCheckService {
 
+	
+	public char findFlames(String name1, String name2) {
+		String temp1 = name1.trim();
+		String temp2 = name2.trim();
+		temp1 = name1.toLowerCase();
+		temp2 = name2.toLowerCase();
+		String temp1_split[] = name1.split(" ");
+		String temp2_split[] = name2.split(" ");
+		temp1 = "";
+		temp2 = "";
+		for (int i = 0; i < temp1_split.length; i++) {
+			temp1 = temp1 + temp1_split[i];
+		}
+		for (int i = 0; i < temp2_split.length; i++) {
+			temp2 = temp2 + temp2_split[i];
+		}
+		StringBuilder sb1 = new StringBuilder(temp1);// converting to string builder
+        StringBuilder sb2 = new StringBuilder(temp2);
+       
+        int m=sb1.length();
+        int n=sb2.length();
+        for(int i=0; i<m;i++)
+        {
+            for(int j=0; j<n;j++)
+            {
+                if(sb1.charAt(i) == sb2.charAt(j))
+                {
+                    sb1.replace(i, i+1, "0"); // replacing matching characters into "0"
+                    sb2.replace(j,j+1,"0");
+                }
+            }
+        }
+ 
+        int x1=0;
+        int y1=0;
+        String s1="";
+        String s2="";
+        s1 = sb1.toString();
+        s2 = sb2.toString();
+            for(int i=0;i<s1.length();i++){ //length of string to remove 0 and find the length
+                if(s1.charAt(i)!='0'){
+                    System.out.print(" "+s1.charAt(i));
+                    x1 +=1;
+                    
+                 }
+            }
+            System.out.println();
+            System.out.println("First String: "+x1);
+    
+            for(int i=0;i<s2.length();i++){
+            if(s2.charAt(i)!='0'){
+                System.out.print(" "+s2.charAt(i));
+                y1 +=1;
+                
+                }
+            } 
+            System.out.println();
+            System.out.println("Second String: "+y1);
+       
+        
+        int x = x1+y1; // total length of remaining characters in both the strings
+        System.out.println("Length is: "+x);
+     
+        String flames = "flames";
+        StringBuilder sb3 = new StringBuilder(flames);
+        
+        char flameResult = 0;
+        
+        while(sb3.length()!=1)
+        {
+            int y = x%sb3.length(); 
+            String temp;
+            
+            if(y!=0)
+            {
+                temp = sb3.substring(y)+sb3.substring(0, y-1); // taking substring (counting purpose)
+                
+            }
+            else
+            {
+                temp = sb3.substring(0, sb3.length()-1); // taking substring (counting purpose)
+                
+            }
+            sb3 = new StringBuilder(temp);
+            flameResult = sb3.charAt(0);
+            
+        }
+        System.out.println(flameResult);
+        
+        switch(flameResult)
+        {
+            case 'f':
+                System.out.println("Friends");
+                break;
+            case 'l':
+                System.out.println("Love");
+                break;
+            case 'a':
+                System.out.println("Affection");
+                break;
+            case 'm':
+                System.out.println("Marriage");
+                break;
+            case 'e':
+                System.out.println("Enemies");
+                break;
+            case 's':
+                System.out.println("Sibling");
+                break;
+                
+        }
+        
+        return flameResult;
+    }
+
+}
